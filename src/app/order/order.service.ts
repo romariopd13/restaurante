329d259c7a6112248cbf78607765792ea7efbd48
Romario Pires <romario.pires@grupolife.med.br>
2021-10-03 22:15:22 -0300

    first commit

---
+++ src/app/order/order.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { Observable } from "rxjs/Observable";
+import { Order, OrderItem } from "./order.model";
+import 'rxjs/add/operator/map'
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { MEAT_API } from "app/app.api";
+
+@Injectable()
+export class OrderService{
+    constructor(private cartService: ShoppingCartService, private http: Http){}
+    
+    itemsValue(): number{
+        return this.cartService.total()
+    }
+    cartItems(): CartItem[]{
+        return this.cartService.items
+    }
+    increaseQty(item: CartItem){
+        this.cartService.increaseQty(item)
+    }
+    decreaseQty(item: CartItem){
+        this.cartService.decreaseQty(item)
+    }
+    remove(item: CartItem){
+        this.cartService.removerItem(item)
+    }
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
+                                                                    .map(response => response.json())
+                                                                    .map(order => order.id)
+    }
+    clear(){
+        this.cartService.clear()
+    }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatinComponent } from "./ratin/ratin.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
+import { SnackbarComponent } from './message/snackbar/snackbar.component';
+import { NotificationService } from "./message/notification.service";
+
+@NgModule({
+    declarations: [InputComponent, RadioComponent, RatinComponent, SnackbarComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    exports: [
+        InputComponent,
+        RadioComponent,
+        RatinComponent,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SnackbarComponent
+    ]
+})
+export class SharedModule {
+    static forRoot(): ModuleWithProviders{
+        return {
+            ngModule: SharedModule,
+            providers: [ 
+                ShoppingCartService, 
+                RestaurantsService,
+                OrderService,
+                NotificationService
+            ]
+        }
+    }
+}
